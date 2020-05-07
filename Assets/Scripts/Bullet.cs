1a4e51a1cc8d5481b03eb49f737d3a9d85ea238c
Angel <angel12345leo@gmail.com>
2020-05-07 11:53:29 +0200

    *

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject impactPrefab;
+    public int lifeTime = 5;
+    [Range(2, 12)] public float speed = 6;
+
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
+    void Update()
+    {
+        transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Environment") || other.gameObject.CompareTag("Ground"))
+        {
+            GameObject impact = Instantiate(impactPrefab, transform.position, transform.rotation);
+            
+            Destroy(impact, 0.25f);
+            Destroy(gameObject);
+        }    
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    [Range(2, 8)] public float speed = 4;
+    [Range(10, 20)] public float jumpForce = 3;
+    [Range(0.005f, 0.5f)] public float fireRate = 0.5f;
+    public ForceMode2D forceMode;
+
+    bool isRunning;
+    int jumpCount;
+    float lastShootTime;
+    SpriteRenderer renderer;
+    Rigidbody2D rigidbody;
+    Animator animator;
+
+    void Start()
+    {
+        renderer = gameObject.GetComponent<SpriteRenderer>();
+        rigidbody = gameObject.GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // <Update Input's>
+        if (Input.GetKey(KeyCode.D))
+        {
+            Move(1);
+        }
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            Move(-1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCount < 2)
+        {
+            Jump();
+        }
+        
+        if (Input.GetKey(KeyCode.Return) && Time.time > fireRate + lastShootTime)
+        {
+            Shoot();
+        }
+
+        // <Update Animator>
+        animator.SetBool("IsRunning", isRunning);
+
+        if (isRunning)
+        {
+            isRunning = false;
+        }
+    }
+
+    void Move(int movement)
+    {
+        isRunning = true;
+        renderer.flipX = movement != 1;
+        var movementVector = new Vector3(movement * speed * Time.deltaTime, 0, 0);
+        transform.Translate(movementVector);
+    }
+
+    void Jump()
+    {
+        jumpCount++;
+        rigidbody.AddForce(new Vector3(0, jumpForce, 0), forceMode);
+        animator.SetTrigger("Jump");
+    }
+
+    void Shoot()
+    {
+        lastShootTime = Time.time;
+        
+        Quaternion rotation = transform.rotation;
+
+        if (renderer.flipX)
+        {
+            rotation = Quaternion.Euler(0, 0, 180);
+        }
+
+        Instantiate(bulletPrefab, transform.position, rotation);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            jumpCount = 0;
+        }
+    }
+}
