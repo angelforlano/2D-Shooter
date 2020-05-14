de9d2d6d1517521ff16a5151460f26c124275e03
Angel <angel12345leo@gmail.com>
2020-05-14 11:33:39 +0200

    + Shoot Effect

---
+++ Assets/Scripts/GameHUD.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameHUD : MonoBehaviour
+{
+    public TextMeshProUGUI bulletsCountText;
+    public Color noBulletsColor;
+    public Color hasBulletsColor;
+
+    public static GameHUD Instance;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    public void UpdateHUD(Player player)
+    {
+        bulletsCountText.text = player.bulletsCount.ToString();
+
+        if (player.bulletsCount > 0)
+        {
+            bulletsCountText.color = hasBulletsColor;
+        } else {
+            bulletsCountText.color = noBulletsColor;
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public GameObject bulletPrefab;
+    public Transform gunPoint;
+    public AudioSource shootEffect;
     [Range(2, 8)] public float speed = 4;
     [Range(10, 20)] public float jumpForce = 3;
+    [Range(0, 100)] public int bulletsCount = 100;
     [Range(0.005f, 0.5f)] public float fireRate = 0.5f;
     public ForceMode2D forceMode;
-
+    
     bool isRunning;
     int jumpCount;
     float lastShootTime;
@@ -22,17 +25,19 @@ public class Player : MonoBehaviour
         renderer = gameObject.GetComponent<SpriteRenderer>();
         rigidbody = gameObject.GetComponent<Rigidbody2D>();
         animator = gameObject.GetComponent<Animator>();
+
+        GameHUD.Instance.UpdateHUD(this);
     }
 
     void Update()
     {
         // <Update Input's>
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.Return))
         {
             Move(1);
         }
         
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.Return))
         {
             Move(-1);
         }
@@ -42,7 +47,7 @@ public class Player : MonoBehaviour
             Jump();
         }
         
-        if (Input.GetKey(KeyCode.Return) && Time.time > fireRate + lastShootTime)
+        if (Input.GetKey(KeyCode.Return) && Time.time > fireRate + lastShootTime && bulletsCount > 0)
         {
             Shoot();
         }
@@ -73,16 +78,35 @@ public class Player : MonoBehaviour
 
     void Shoot()
     {
+        bulletsCount--;
+        animator.SetTrigger("Shoot");
+        shootEffect.pitch = Random.Range(1.5f, 1.95f);
+        shootEffect.Play();
+
         lastShootTime = Time.time;
         
         Quaternion rotation = transform.rotation;
+        Vector3 position = gunPoint.position;
 
         if (renderer.flipX)
         {
             rotation = Quaternion.Euler(0, 0, 180);
+            position = new Vector3(transform.position.x + (gunPoint.localPosition.x * -1), gunPoint.position.y, gunPoint.position.z);
         }
+        
+        Instantiate(bulletPrefab, position, rotation);
 
-        Instantiate(bulletPrefab, transform.position, rotation);
+        GameHUD.Instance.UpdateHUD(this);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PowerUp"))
+        {
+            bulletsCount += 75;
+            GameHUD.Instance.UpdateHUD(this);
+            Destroy(other.gameObject);
+        }
     }
 
     void OnCollisionEnter2D(Collision2D other)
